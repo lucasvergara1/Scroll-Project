484300afccc78e9b4fa1c2183ce9c99890702ae0
Lucas <lucas.vergara@live.com>
2024-02-19 22:10:33 -0300

    :construction: Create smooth scroll and precise scrolling
    As the user scroll down the page and click on the top menu, the system displays the page precisely, if you click on services it will go precisely to services even if the user has scrolled the page a little towards about.

--- app.js
+++ app.js
@@ -51,4 +51,36 @@ window.addEventListener('scroll', function() {
 });
 
 // ********** smooth scroll ************
+
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link){
+    link.addEventListener('click', function (e) {
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot  
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+
+        let position = element.offsetTop - navHeight;
+
+        if (!fixedNav){
+            position = position - navHeight;
+        }
+        if (navHeight > 83) {
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left:0, 
+            top:position,
+        });
+        linksContainer.style.height = 0;
+
+    });
+});
